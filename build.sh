f4498b70c9bbcecc9c5f60e5cdb52a4caa1d7658
Marcus Weiner <marcus.weiner@gmail.com>
2021-01-18 01:22:00 +0100

    ✨ Build a front image

--- build.sh
+++ build.sh
@@ -10,3 +10,5 @@ docker build common --target proto -t ffddorf/workadventure-proto
 docker build nodejs -t ffddorf/workadventure-back --build-arg SERVICE=back --build-arg BASE_LAYER=proto
 docker build nodejs -t ffddorf/workadventure-pusher --build-arg SERVICE=pusher --build-arg BASE_LAYER=proto
 docker build nodejs -t ffddorf/workadventure-uploader --build-arg SERVICE=uploader
+
+docker build front -t ffddorf/workadventure-front
