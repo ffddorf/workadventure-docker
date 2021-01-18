74fd6bc6018011e3ac1b22e4c491e8dc2ce4f238
Marcus Weiner <marcus.weiner@gmail.com>
2021-01-18 01:07:12 +0100

    ✨ Build back, pusher & uploader from same dockerfile

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -ex -o pipefail
+
+export DOCKER_BUILDKIT=1
+
+docker build common --target base -t ffddorf/workadventure-base
+docker build common --target proto -t ffddorf/workadventure-proto
+
+docker build nodejs -t ffddorf/workadventure-back --build-arg SERVICE=back --build-arg BASE_LAYER=proto
+docker build nodejs -t ffddorf/workadventure-pusher --build-arg SERVICE=pusher --build-arg BASE_LAYER=proto
+docker build nodejs -t ffddorf/workadventure-uploader --build-arg SERVICE=uploader
