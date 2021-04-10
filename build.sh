550c4b001be5576860c240bf3ab84954d6cce269
Marcus Weiner <marcus.weiner@gmail.com>
2021-04-10 23:26:28 +0200

    Configure the image cascade in build script

--- build.sh
+++ build.sh
@@ -4,11 +4,19 @@ set -ex -o pipefail
 
 export DOCKER_BUILDKIT=1
 
-docker build common --target base -t ffddorf/workadventure-base
-docker build common --target proto -t ffddorf/workadventure-proto
+GIT_REPO="https://github.com/kalkspace/workadventure.git"
+GIT_BRANCH="develop"
+GIT_COMMIT="HEAD"
 
-docker build nodejs -t ffddorf/workadventure-back --build-arg SERVICE=back --build-arg BASE_LAYER=proto
-docker build nodejs -t ffddorf/workadventure-pusher --build-arg SERVICE=pusher --build-arg BASE_LAYER=proto
-docker build nodejs -t ffddorf/workadventure-uploader --build-arg SERVICE=uploader
+IMAGE_PREFIX="quay.io/kalkspace/workadventure"
+IMAGE_TAG="local-pusher-2"
 
-docker build front -t ffddorf/workadventure-front
+docker build common --target base -t "workadventure-base" \
+  --build-arg GIT_REPO=${GIT_REPO} --build-arg GIT_BRANCH=${GIT_BRANCH} --build-arg GIT_COMMIT=${GIT_COMMIT}
+docker build common --target proto -t "workadventure-proto"
+
+docker build nodejs -t "${IMAGE_PREFIX}-back:${IMAGE_TAG}" --build-arg SERVICE=back --build-arg BASE_LAYER=proto
+docker build nodejs -t "${IMAGE_PREFIX}-pusher:${IMAGE_TAG}" --build-arg SERVICE=pusher --build-arg BASE_LAYER=proto
+docker build nodejs -t "${IMAGE_PREFIX}-uploader:${IMAGE_TAG}" --build-arg SERVICE=uploader
+
+docker build front -t "${IMAGE_PREFIX}-front:${IMAGE_TAG}"
