93c7f7becc6b6a7aa79c12ab38bb9873939beda2
Mic Szillat <mic@nomaster.cc>
2021-07-21 22:48:06 +0200

    :bug: Work around cache issues by disabling caching

--- build.sh
+++ build.sh
@@ -4,10 +4,10 @@ set -ex -o pipefail
 
 export DOCKER_BUILDKIT=1
 
-docker build common --tag ffddorf/workadventure-base  --target base
-docker build common --tag ffddorf/workadventure-proto --target proto
-docker build nodejs --tag quay.io/ffddorf/workadventure-back:latest   --progress=plain --build-arg SERVICE=back   --build-arg BASE_LAYER=proto
-docker build nodejs --tag quay.io/ffddorf/workadventure-pusher:latest --progress=plain --build-arg SERVICE=pusher --build-arg BASE_LAYER=proto
-docker build front  --tag quay.io/ffddorf/workadventure-front:latest  --progress=plain
+docker build common --no-cache --tag ffddorf/workadventure-base  --target base
+docker build common --no-cache --tag ffddorf/workadventure-proto --target proto
+docker build nodejs --no-cache --tag quay.io/ffddorf/workadventure-back:latest   --progress=plain --build-arg SERVICE=back   --build-arg BASE_LAYER=proto
+docker build nodejs --no-cache --tag quay.io/ffddorf/workadventure-pusher:latest --progress=plain --build-arg SERVICE=pusher --build-arg BASE_LAYER=proto
+docker build front  --no-cache --tag quay.io/ffddorf/workadventure-front:latest  --progress=plain
 # we don't need this service atm
-# docker build nodejs --tag quay.io/ffddorf/workadventure-uploader:latest --progress=plain --build-arg SERVICE=uploader
+# docker build nodejs --no-cache --tag quay.io/ffddorf/workadventure-uploader:latest --progress=plain --build-arg SERVICE=uploader
